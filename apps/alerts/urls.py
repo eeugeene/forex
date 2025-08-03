4999bf37441003f43d1777ecb478a822a564ad2e
eeugeene <101885380+eeugeene@users.noreply.github.com>
2025-08-03 16:36:01 +0300

    apps/alerts/urls.py

---
+++ apps/alerts/urls.py
@@ -0,0 +1,11 @@
+# ===== apps/alerts/urls.py =====
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AlertViewSet
+
+router = DefaultRouter()
+router.register(r'', AlertViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
